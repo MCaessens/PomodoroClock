2b98113039a08903fcfa97774b6ae57c5b595a92
Marco Caessens <marco.caessens@diekeure.be>
2023-01-17 20:52:25 +0100

    Edit TimeBar

--- Assets/Scripts/PomodoroManager.cs
+++ Assets/Scripts/PomodoroManager.cs
@@ -12,24 +12,29 @@ public class PomodoroManager : MonoBehaviour
 {
     #region Variables & Properties
 
-    private enum State
+    public enum State
     {
-        Work,
         Focus,
-        Idle
+        Break,
+        Idle,
+        Paused
     }
 
-    [SerializeField] private TMP_InputField timeText;
+    [SerializeField] private TMP_InputField timeInput;
+    [SerializeField] private TextMeshProUGUI timeLabel;
+    [SerializeField] private TextMeshProUGUI stateLabel;
     [SerializeField] private Button startButton;
     [SerializeField] private Button pauseButton;
+    [SerializeField] private Button addFiveMinButton;
     [SerializeField] private TimeBar timeBar;
 
     [SerializeField] private float timeRemainingInSeconds;
     [SerializeField] private float timeGoalInSeconds;
 
     private bool _isFirstTime;
+    private bool _isEndTriggered;
     private State _currentState;
-    private State? _previousState;
+    private State _previousState;
 
     #endregion
 
@@ -38,13 +43,13 @@ public class PomodoroManager : MonoBehaviour
     private void Awake()
     {
         _currentState = State.Idle;
-        _previousState = null;
+        _previousState = State.Idle;
         _isFirstTime = true;
     }
 
     private void Update()
     {
-        if (_currentState == State.Idle) return;
+        if (_currentState is State.Idle or State.Paused) return;
 
         UpdateRemainingTime();
     }
@@ -55,36 +60,60 @@ public class PomodoroManager : MonoBehaviour
 
     public void StartTimer()
     {
+        if (_currentState == State.Paused)
+        {
+            _currentState = _previousState;
+            HandleStartUI();
+            return;
+        }
+        _currentState =  _previousState == State.Break || _isFirstTime ? State.Focus : State.Break;
+        Debug.Log(_currentState);
+        
         if (timeRemainingInSeconds == 0)
         {
-            var isInputValid = float.TryParse(timeText.text, out var timeInput);
-            if (!isInputValid) return;
+            if (_currentState == State.Break)
+            {
+                timeRemainingInSeconds = 600f;
+                timeGoalInSeconds = 600f;
+            }
+            else
+            {
+                var isInputValid = float.TryParse(this.timeInput.text, out var timeInput);
+                if (!isInputValid) return;
 
-            timeInput *= 60f;
-            timeRemainingInSeconds = timeInput;
-            timeGoalInSeconds = timeInput;
+                timeInput *= 60f;
+                timeRemainingInSeconds = timeInput;
+                timeGoalInSeconds = timeInput;
+            }
         }
 
         HandleStartUI();
-        _currentState = _previousState == State.Focus || _isFirstTime ? State.Work : State.Focus;
         if (_isFirstTime) _isFirstTime = false;
     }
 
     public void PauseTimer()
     {
-        HandlePauseUI();
-
         _previousState = _currentState;
-        _currentState = State.Idle;
+        _currentState = State.Paused;
+        
+        HandlePauseUI();
     }
 
     public void ResetTimer()
     {
-        HandleResetUI();
-
         _currentState = State.Idle;
         timeRemainingInSeconds = 0;
         timeGoalInSeconds = 0;
+        timeLabel.text = "00:00";
+        _isEndTriggered = false;
+
+        HandleResetUI();
+    }
+
+    public void AddFiveMinutesToTimer()
+    {
+        timeRemainingInSeconds += 300f;
+        timeGoalInSeconds += 300f;
     }
 
     #endregion
@@ -93,9 +122,11 @@ public class PomodoroManager : MonoBehaviour
 
     private void HandleStartUI()
     {
-        timeText.interactable = false;
+        timeInput.interactable = false;
+        stateLabel.text = $"{_currentState.ToString().ToUpper()} TIME";
         startButton.interactable = false;
         pauseButton.interactable = true;
+        addFiveMinButton.interactable = true;
     }
 
     private void HandlePauseUI()
@@ -108,12 +139,19 @@ public class PomodoroManager : MonoBehaviour
     {
         startButton.interactable = true;
         pauseButton.interactable = false;
-        timeText.interactable = true;
-        timeText.text = "";
+        timeInput.interactable = true;
+        addFiveMinButton.interactable = true;
+        timeBar.ResetTimeBar();
     }
 
     private void UpdateRemainingTime()
     {
+        if (timeRemainingInSeconds <= 7 && !_isEndTriggered)
+        {
+            _isEndTriggered = true;
+            AudioManager.Instance.StartEndSounds(7, _currentState);
+        }
+
         if (timeRemainingInSeconds <= 0)
         {
             ResetTimer();
@@ -121,7 +159,7 @@ public class PomodoroManager : MonoBehaviour
         }
 
         timeRemainingInSeconds -= Time.deltaTime;
-        timeText.text = FormatTime(timeRemainingInSeconds);
+        timeLabel.text = FormatTime(timeRemainingInSeconds);
         timeBar.CalculateTimeBar(timeRemainingInSeconds, timeGoalInSeconds);
     }
 
@@ -131,13 +169,7 @@ public class PomodoroManager : MonoBehaviour
         var timeSpan = TimeSpan.FromSeconds(timeInSeconds);
         var minutes = timeSpan.Minutes;
         var seconds = timeSpan.Seconds;
-
-        if (minutes == 0)
-        {
-            stringBuilder.Append(seconds);
-            return stringBuilder.ToString();
-        }
-
+        
         stringBuilder.Append(AddNumberInsert(minutes));
         stringBuilder.Append($"{minutes}:");
 
@@ -151,11 +183,8 @@ public class PomodoroManager : MonoBehaviour
     {
         return number > 9 ? "" : "0";
     }
-
-    private static string AddSemiColonInsert(float minutes, float seconds)
-    {
-        return minutes == 1 && seconds == 0 ? "" : ":";
-    }
+    
+    
 
     #endregion
 }
